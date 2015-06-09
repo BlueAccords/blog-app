d669fe855dba98b4e33a44274dba9a654e63256d
BlueAccords <myoldemailname@gmail.com>
2015-06-09 00:19:31 +0000

    Edit and delete posts, no restrictions, minor tests for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 4, maximum: 140 }
+  validates :title, presence: true, length: { minimum: 4 }
   validates :body, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @post = Post.new(title: "test title", body: "test body")
+  end
+  
+  test "post should be valid" do
+    assert @post.valid?
+  end
+  
+  test "post title should be present, invalid post" do
+    @post.title = nil
+    assert_not @post.valid?
+  end
+    
+  test "post content should be present" do
+    @post.body = "   "
+    assert_not @post.valid?
+  end
+    
+  test "post title should be 4 minimum characters" do
+    @post.title = "not"
+    assert_not @post.valid?
+  end
+  
+    
 end
